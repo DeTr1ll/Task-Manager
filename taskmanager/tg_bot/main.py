a95e958b7a2db421bfb3def931c31eb9240034bb
DeTr1ll <illyatr25@gmail.com>
2025-08-14 10:58:05 +0300

    1

--- taskmanager/tg_bot/main.py
+++ taskmanager/tg_bot/main.py
@@ -5,7 +5,7 @@ TOKEN = config("TELEGRAM_BOT_TOKEN")
 if not TOKEN:
     raise RuntimeError("TELEGRAM_BOT_TOKEN not set")
 
-WEBHOOK_URL = config("WEBHOOK_URL") or f"https://{config('RENDER_EXTERNAL_HOSTNAME')}/bot/{TOKEN}/"
+WEBHOOK_URL = f"https://{config('RENDER_EXTERNAL_HOSTNAME')}/bot/{TOKEN}/"
 
 print("Setting webhook to", WEBHOOK_URL)
 r = requests.post(f"https://api.telegram.org/bot{TOKEN}/setWebhook", json={"url": WEBHOOK_URL, "allowed_updates": ["message", "callback_query"]}, timeout=15)
