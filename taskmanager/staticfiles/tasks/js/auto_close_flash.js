381c012ff6dd86e30676d3e9f84023b6ac55658a
Illia <33633382+DeTr1ll@users.noreply.github.com>
2025-08-19 10:00:59 +0300

    Delete taskmanager/staticfiles directory

--- taskmanager/staticfiles/tasks/js/auto_close_flash.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(function () {
-    const alerts = document.querySelectorAll('.flash-message');
-    alerts.forEach(function (alert) {
-      const alertInstance = bootstrap.Alert.getOrCreateInstance(alert);
-      alertInstance.close();
-    });
-  }, 5000);
-});
